4f63bd5d16708ecce6393352a0a9aef0885a11b8
Ana-Bruginski <bruginskiana06@gmail.com>
2026-09-17 18:00:47 -0300

    Versão final

--- src/ArvoreBinaria.java
+++ src/ArvoreBinaria.java
@@ -1,14 +1,11 @@
-import javax.xml.transform.Source;
-import java.sql.SQLOutput;
+public class ArvoreBinaria {
 
-public class ArvoreBinaria{
-
-    class No{
+    class No {
         int dado;
         No esquerda;
         No direita;
 
-        No(int elemento){
+        No(int elemento) {
             this.dado = elemento;
             this.esquerda = null;
             this.direita = null;
@@ -17,24 +14,24 @@ public class ArvoreBinaria{
 
     No raiz = null;
 
-    public void insereElemento(int elemento){
+    public void insereElemento(int elemento) {
         No novoNo = new No(elemento);
         No atual = raiz;
 
-        if (raiz == null){
+        if (raiz == null) {
             raiz = novoNo;
             System.out.println("Inserido: " + elemento);
-        } else{
-            while(true){
-                if (elemento < atual.dado){
-                    if (atual.esquerda == null){
+        } else {
+            while (true) {
+                if (elemento < atual.dado) {
+                    if (atual.esquerda == null) {
                         atual.esquerda = novoNo;
                         System.out.println("Inserido: " + elemento);
                         return;
                     }
                     atual = atual.esquerda;
-                } else{
-                    if (atual.direita == null){
+                } else {
+                    if (atual.direita == null) {
                         atual.direita = novoNo;
                         System.out.println("Inserido: " + elemento);
                         return;
@@ -45,19 +42,81 @@ public class ArvoreBinaria{
         }
     }
 
-    public void removeElemento(int elemento){
-
-    }
-
-    public void buscaElemento(int elemento){
-
-    }
-
-    public void exibeArvore(){
+    public void buscaElemento(int elemento) {
         No atual = raiz;
+
+        while (atual != null) {
+            if (elemento == atual.dado) {
+                System.out.println("Encontrado: " + elemento);
+                return;
+            }
+            if (elemento < atual.dado) {
+                atual = atual.esquerda;
+            } else {
+                atual = atual.direita;
+            }
+        }
+        System.out.println("Elemento nao encontrado: " + elemento);
     }
 
-    public static void main(String[] args){
+    public void removeElemento(int elemento) {
+        No atual = raiz;
+        No pai = null;
+
+        while (atual != null && atual.dado != elemento) {
+            pai = atual;
+            if (elemento < atual.dado) {
+                atual = atual.esquerda;
+            } else {
+                atual = atual.direita;
+            }
+        }
+
+        if (atual == null) {
+            System.out.println("Elemento nao encontrado: " + elemento);
+            return;
+        }
+
+        if (atual.esquerda != null && atual.direita != null) {
+            No paiSubstituto = atual;
+            No substituto = atual.direita;
+
+            while (substituto.esquerda != null) {
+                paiSubstituto = substituto;
+                substituto = substituto.esquerda;
+            }
+
+            atual.dado = substituto.dado;
+            atual = substituto;
+            pai = paiSubstituto;
+        }
+
+        No filho = atual.esquerda;
+        if (filho == null) {
+            filho = atual.direita;
+        }
+
+        if (pai == null) {
+            raiz = filho;
+        } else if (pai.esquerda == atual) {
+            pai.esquerda = filho;
+        } else {
+            pai.direita = filho;
+        }
+
+        System.out.println("Removido: " + elemento);
+    }
+
+    public void exibeArvore(No no) {
+        if (no == null) {
+            return;
+        }
+        exibeArvore(no.esquerda);
+        System.out.print(no.dado + " ");
+        exibeArvore(no.direita);
+    }
+
+    public static void main(String[] args) {
         ArvoreBinaria arvore = new ArvoreBinaria();
 
         arvore.insereElemento(50);
@@ -65,6 +124,21 @@ public class ArvoreBinaria{
         arvore.insereElemento(70);
         arvore.insereElemento(20);
         arvore.insereElemento(40);
-        arvore.insereElemento(35);
+        arvore.insereElemento(60);
+        arvore.insereElemento(80);
+
+        arvore.exibeArvore(arvore.raiz);
+        System.out.println();
+
+        arvore.buscaElemento(40);
+        arvore.buscaElemento(99);
+
+        arvore.removeElemento(20);
+        arvore.removeElemento(30);
+        arvore.removeElemento(50);
+        arvore.removeElemento(99);
+
+        arvore.exibeArvore(arvore.raiz);
+        System.out.println();
     }
 }
